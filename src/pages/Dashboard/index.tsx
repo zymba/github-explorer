55cff238f1c059b2333c1de067e32fb64b4a0f5a
Bueno <zymbabueno@gmail.com>
2020-04-24 19:58:23 -0300

    Estilos da página dashboard. Adiciona os pacotes polished e react-icons

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,63 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="#newrepo">
+          <img
+            src="https://avatars1.githubusercontent.com/u/58253?s=460&u=a036f1d0b030a19e05692d1a61b4c27afafc5b6d&v=4"
+            alt="Felipe Bueno"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="#newrepo">
+          <img
+            src="https://avatars1.githubusercontent.com/u/58253?s=460&u=a036f1d0b030a19e05692d1a61b4c27afafc5b6d&v=4"
+            alt="Felipe Bueno"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="#newrepo">
+          <img
+            src="https://avatars1.githubusercontent.com/u/58253?s=460&u=a036f1d0b030a19e05692d1a61b4c27afafc5b6d&v=4"
+            alt="Felipe Bueno"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
