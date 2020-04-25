af8bf1bf67e682962e68e514fc0d23d9242e6d62
Bueno <zymbabueno@gmail.com>
2020-04-25 15:12:24 -0300

    Estilos da página Repository

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+import logoImg from '../../assets/logo.svg';
 
 interface RepositoryParams {
   repository: string;
@@ -8,7 +12,56 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository: {params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars3.githubusercontent.com/u/69631?v=4"
+            alt="avatar"
+          />
+          <div>
+            <strong>Titulo do reositorio</strong>
+            <p>descricao do repositorio</p>
+          </div>
+        </header>
+
+        <ul>
+          <li>
+            <strong>1840</strong>
+            <span>stars</span>
+          </li>
+          <li>
+            <strong>1840</strong>
+            <span>stars</span>
+          </li>
+          <li>
+            <strong>1840</strong>
+            <span>stars</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="/repositories/hg">
+          <div>
+            <strong>sdfsfs</strong>
+            <p>kjhjkhkjh</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
